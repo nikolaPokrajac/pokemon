31c585c0e07d19a10100f06d91a6de32b81e1d54
nikolaPokrajac <nikolapokrajac92@gmail.com>
2019-07-11 13:08:01 +0200

    first commit

---
+++ src/Pokecard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Pokecard.css';
+
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+// pokemon ID for images must be padded with zeros to three places, change 4 into 004 etc
+let padToThree = number => (number <= 999 ? `00${number}`.slice(-3) : number);
+
+class PokeCard extends Component {
+  render() {
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
+    return (
+      <div className="Pokecard">
+        <h1 className="Pokecard-title">{this.props.name}</h1>
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">EXP: {this.props.exp}</div>
+      </div>
+    );
+  }
+}
+
+export default PokeCard;
